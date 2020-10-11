d381b8106eeca8dbe859c24ce23963858fcaee52
angels101 <angelscodex101@gmail.com>
2020-10-11 07:52:44 +0300

    adding the dynamic urlss on the gallery url.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import welcome
-
+from .views import welcome, gallery_today, dynamic_urls
+# from django.conf.urls import url
 urlpatterns = [
     path('', welcome, name='home')
+    path('gallery-today/', gallery_today, name='galleryToday'),
+    path('dynamic/<slug:query>/'. dynamic_urls, name='dynamic'),
 ]
