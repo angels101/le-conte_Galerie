8b5e5cca947c748f823e4dcb777c9eb1e3c6e2d1
angels101 <angelscodex101@gmail.com>
2020-10-11 08:40:29 +0300

    adding of the cloudinary image

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from cloudinary import CloudinaryImage
 # Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length=100)
+    image = CloudinaryImage('image')
+    
