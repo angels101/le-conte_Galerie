add71d3c4f48870e6cca81138a6fea6d9b488dda
angels101 <angelscodex101@gmail.com>
2021-03-23 08:49:28 +0300

    search and location urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,10 @@ from django.urls import path,include
 
 urlpatterns = [
     path('', views.main_flikr,name='main_flikr'),
+    path('location/(\d+',views.location, name='location'),
+    path('search/',views.search, name='search')
    
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
