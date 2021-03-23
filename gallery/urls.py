cdb9187c06e76cae538c4558d5c45866bf1e71c9
angels101 <angelscodex101@gmail.com>
2021-03-23 11:52:17 +0300

    urls for flikr and gallery

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,10 +1,12 @@
 from .import views
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.main_flikr,name='main_flikr'),
     path('location/(\d+',views.location, name='location'),
-    path('search/',views.search, name='search')
+    path('search/',views.search, name='search'),
    
 ]
 if settings.DEBUG:
