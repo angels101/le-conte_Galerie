6bd3641ece83a928ca5652d800199aa3499e3c71
angels101 <angelscodex101@gmail.com>
2021-03-23 04:59:15 +0300

    galllerty urls and views

--- flikr/settings.py
+++ flikr/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
@@ -76,7 +73,7 @@ WSGI_APPLICATION = 'flikr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
---
+++ gallery/urls.py
@@ -0,0 +1,7 @@
+from .import views
+from django.urls import path,include
+
+urlpatterns = [
+    path('', views.main_flikr,name='main_flikr'),
+    path('', include(gallery.urls)),
+]
