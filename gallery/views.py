f395e492b5aeb2c6f82897f33ed9894f085d7fa7
angels101 <angelscodex101@gmail.com>
2021-03-23 05:20:38 +0300

    views for location and serch images definition

--- gallery/views.py
+++ gallery/views.py
@@ -5,4 +5,16 @@ from .models import Image, Location
 def main_flikr(request):
     images= Image.all_images()
     location = Location.objects.all()
-    return render(request, 'index.html',)
+    return render(request, 'index.html',{"images":images,"locations":location})
+
+def location(request,location):
+    locations = Location.objects.all()
+    selected_location = Location.objects.get(id = location)
+    images = Image.objects.filter(location = selected_location.id)
+    return render(request, 'location.html', {"location":selected_location,"locations":locations,"images":images})
+
+def search(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_images = Image.search_by_category(search_term)
+    return render(request,'search.html',{"images":searched_images,"category":search_term})
