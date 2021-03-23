5161453edada502a7265aa03155cc7f977d04017
angels101 <angelscodex101@gmail.com>
2021-03-23 05:14:56 +0300

    views for main flikr image, defined location

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,Http404
+from .models import Image, Location
 # Create your views here.
+def main_flikr(request):
+    images= Image.all_images()
+    location = Location.objects.all()
+    return render(request, 'index.html',)
