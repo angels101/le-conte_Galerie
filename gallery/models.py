003512199693211b23c1d50b7f118d000d3f011b
angels101 <angelscodex101@gmail.com>
2021-04-11 09:51:12 +0300

    location view

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+import cloudinary
+from cloudinary.models import CloudinaryField 
+import datetime as dt 
 # Create your models here.
+class categories(models.Model):
+    name = models.CharField(max_length=30)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse,Http404
+from .models import Image, Location
+import datetime as dt 
 
 # Create your views here.
+def main_flikr(request):
+    images= Image.all_images()
+    location = Location.objects.all()
+    return render(request,'index.html',{"images":images,"locations":location})
+
+def location(request,location):
+    locations = Location.objects.all()
+    selected_location = Location.objects.get(id = location)
+    images = Image.objects.filter(location = selected_location.id)
+    return render(request, 'location.html', {"location":selected_location,"locations":locations,"images":images})
