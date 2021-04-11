e802711fe687638a88f3ae383b5208a44a1c135f
angels101 <angelscodex101@gmail.com>
2021-04-11 09:45:58 +0300

    app galllery url patterns

--- flikr/urls.py
+++ flikr/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('gallery.urls')),
-]
+] #if settings.DEBUG:
+   # urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
---
+++ gallery/urls.py
@@ -0,0 +1,11 @@
+from .import views
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.main_flikr,name='main_flikr'),
+    path('location/<str:location>/',views.location, name='location'),
+    path('search/',views.search, name='search'),
+   
+]
